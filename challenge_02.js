30e2934fe4883b3bdac5ef917f652aec9b3f8bd6
GoloMeyer84 <golomeyer84@googlemail.com>
2022-01-18 12:28:06 +0100

    Challenge gelöst

--- challenge_02.js
+++ challenge_02.js
@@ -15,5 +15,28 @@ Alter 13-17 // Cola
 --> "Max trinkt Cola."
 
 /* Getränke-Challenge */
+const cond = true; //um eine Wahrheitsprüfung durchzuführen muss eine cond - Condition = TRUE genutzt werden
+ let firstName, lastName, alter;
 
- 
+firstName   = prompt("Bitte geben Sie Ihren Vornamen ein");
+lastName    = prompt("Bitte geben Sie Ihren Nachnamen ein");
+alter       = parseInt(prompt("Bitte geben Sie Ihr Alter ein")); //umwandlung String in INT (scheint aber auch ohne zu gehen)
+// parseInt(alter);
+
+switch (cond) {
+    case alter >= 0 && alter < 6 : // UND Operator ist && 
+        console.log(firstName + " " + lastName + " " + "trinkt Milch");
+        break;
+    case alter >= 6 && alter <= 12 :
+        console.log(firstName + " " + lastName + " " + "trinkt Saft");
+        break;
+    case alter >= 13 && alter <= 17:
+        console.log(firstName + " " + lastName + " " + "trinkt Cola");
+        break;
+    case alter >= 18 :
+        console.log(firstName + " " + lastName + " " + "trinkt Wein");
+        break;
+    default:
+        console.log(firstName + " " + lastName + " " + "trinkt nichts")
+        break;
+}
